8d52599ddcf661a260e6e90ae95725d50b574069
anonymous-90 <kubakusa1994@gmail.com>
2026-09-09 16:02:16 +0100

    Keybind register fixed

--- src/main/java/net/gamer/consumableblocks/keymapping/ModKeymappings.java
+++ src/main/java/net/gamer/consumableblocks/keymapping/ModKeymappings.java
@@ -1,6 +1,7 @@
 package net.gamer.consumableblocks.keymapping;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.gamer.consumableblocks.ConsumableBlocks;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
@@ -8,7 +9,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class ModKeymappings {
 
-    public static final KeyMapping SmeltItem = new KeyMapping("key.consumable_blocks_key", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_B, KeyMapping.Category.GAMEPLAY);
+    public static final KeyMapping SmeltItem = KeyMappingHelper.registerKeyMapping( new KeyMapping("key.consumable_blocks_key", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_B, KeyMapping.Category.MISC));
 
     public  static void Register(){
         ConsumableBlocks.LOGGER.info("registering keybinds for" + ConsumableBlocks.MOD_ID);
